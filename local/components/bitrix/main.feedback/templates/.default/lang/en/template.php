f7a71371618f59708da6ea7d227e45159cf737c6
Daniil Zemnov <zemnowf@gmail.com>
2023-03-16 15:12:41 +0300

    add feedback form via class controller add validator add response component

---
+++ local/components/bitrix/main.feedback/templates/.default/lang/en/template.php
@@ -0,0 +1,12 @@
+<?
+$MESS ['MFT_NAME'] = "Name";
+$MESS ['MFT_SURNAME'] = "Surname";
+$MESS ['MFT_COMPANY'] = "Company and position";
+$MESS ['MFT_MESSAGE'] = "Message";
+$MESS ['MFT_DEPARTMENT'] = "Department";
+$MESS ['MFT_DEPARTMENT_SALES'] = "Sales department";
+$MESS ['MFT_DEPARTMENT_SUPPORT'] = "Support department";
+$MESS ['MFT_DEPARTMENT_EDU'] = "Educational department";
+$MESS ['MFT_DEPARTMENT_SUPPLIES'] = "Supplies department";
+$MESS ['MFT_SUBMIT'] = "Send";
+?>
